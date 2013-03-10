990b09baf584b5578c0da716f5e9ee04450f20b9
nhthien <nhthien@jaist.ac.jp>
2013-03-09 20:13:06 -0800

    Initial Core Data

---
+++ Slide48/Slide48/PuzzleBoard.h
@@ -0,0 +1,17 @@
+//
+//  PuzzleBoard.h
+//  Slide48
+//
+//  Created by Thien Nguyen on 3/9/13.
+//  Copyright (c) 2013 Pulkit Kathuria. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface PuzzleBoard : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * size;
+
+@end
---
+++ Slide48/Slide48/PuzzleBoard.m
@@ -0,0 +1,16 @@
+//
+//  PuzzleBoard.m
+//  Slide48
+//
+//  Created by Thien Nguyen on 3/9/13.
+//  Copyright (c) 2013 Pulkit Kathuria. All rights reserved.
+//
+
+#import "PuzzleBoard.h"
+
+
+@implementation PuzzleBoard
+
+@dynamic size;
+
+@end
